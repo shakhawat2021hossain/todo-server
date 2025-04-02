08f2d5a2912aa70d72bf25a27a84f0fb7d9f68fc
Shakhawat <shakhawat2021hossain2021@gmail.com>
2025-04-02 20:32:52 +0600

    completed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const cors = require("cors")
 
 const swaggerUI = require("swagger-ui-express")
 const swaggerDoc = require('./swagger/swagger')
+const CSS_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+
 
 const app = express()
 app.use(cors())
@@ -11,13 +14,18 @@ app.use(express.json())
 const todoRoutes = require('./routes/todoRoutes')
 const userRoutes = require('./routes/userRoutes')
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("hello from server")
 })
 
 app.use('/api/todos', todoRoutes)
 app.use('/api/auth', userRoutes)
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc,
+    {
+        customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL
+    }
+))
 
 module.exports = app
